2f77029ffec0bbe707b2f3e32319656eada12dad
Nenko Tabakov <nenko.tabakov@gmail.com>
2022-12-21 10:19:42 +0000

    feat: Added shorthands for common mapping functions

---
+++ src/main/java/neta/mappers/Mappers.java
@@ -0,0 +1,52 @@
+package neta.mappers;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public final class Mappers {
+
+    public static <T, R> List<R> map(List<T> toMap, Function<T, R> mapper) {
+        return mappedStream(toMap, mapper).collect(Collectors.toUnmodifiableList());
+    }
+
+    public static <T, R> Set<R> map(Set<T> toMap, Function<T, R> mapper) {
+        return mappedStream(toMap, mapper).collect(Collectors.toUnmodifiableSet());
+    }
+
+    public static <T, K, V> Map<K, V> associate(Collection<T> toAssociate, Function<T, K> keyMapper, Function<T, V> valueMapper) {
+        return toAssociate.stream().collect(Collectors.toUnmodifiableMap(keyMapper, valueMapper));
+    }
+
+    public static <T, K> Map<K, T> associateBy(Collection<T> toAssociate, Function<T, K> keyMapper) {
+        return associate(toAssociate, keyMapper, Function.identity());
+    }
+
+    public static <T, V> Map<T, V> associateWith(Collection<T> toAssociate, Function<T, V> valueMapper) {
+        return associate(toAssociate, Function.identity(), valueMapper);
+    }
+
+    public static <T, K> Map<K, List<T>> groupBy(List<T> toGroup, Function<T, K> groupBy) {
+        return toGroup.stream().collect(Collectors.groupingBy(groupBy, Collectors.toUnmodifiableList()));
+    }
+
+    public static <T, K> Map<K, Set<T>> groupBy(Set<T> toGroup, Function<T, K> groupBy) {
+        return toGroup.stream().collect(Collectors.groupingBy(groupBy, Collectors.toUnmodifiableSet()));
+    }
+
+    public static <T, R> List<R> flatten(List<T> toFlatten, Function<T, Stream<R>> mapper) {
+        return toFlatten.stream().flatMap(mapper).collect(Collectors.toUnmodifiableList());
+    }
+
+    public static <T, R> Set<R> flatten(Set<T> toFlatten, Function<T, Stream<R>> mapper) {
+        return toFlatten.stream().flatMap(mapper).collect(Collectors.toUnmodifiableSet());
+    }
+
+    private static <T, R> Stream<R> mappedStream(Collection<T> toMap, Function<T, R> mapFunction) {
+        return toMap.stream().map(mapFunction);
+    }
+}
---
+++ src/test/java/MappersTest.java
@@ -0,0 +1,84 @@
+import neta.mappers.Mappers;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MappersTest {
+
+    @Test
+    public void mapList() {
+        var mappedList = Mappers.map(List.of(1, 2, 3), it -> String.valueOf(it));
+        var expectedList = List.of("1", "2", "3");
+
+        assertEquals(expectedList, mappedList);
+    }
+
+    @Test
+    public void mapSet() {
+        var mappedSet = Mappers.map(Set.of(1, 2, 3), it -> String.valueOf(it));
+        var expectedSet = Set.of("1", "2", "3");
+
+        assertEquals(expectedSet, mappedSet);
+    }
+
+    @Test
+    public void associate() {
+        var associated = Mappers.associate(List.of(1, 2, 3), it -> String.valueOf(it), it -> it * 10);
+        var expected = Map.of("1", 10, "2", 20, "3", 30);
+
+        assertEquals(expected, associated);
+    }
+
+    @Test
+    public void associateBy() {
+        var associated = Mappers.associateBy(List.of(1, 2, 3), it -> String.valueOf(it));
+        var expected = Map.of("1", 1, "2", 2, "3", 3);
+
+        assertEquals(expected, associated);
+    }
+
+    @Test
+    public void associateWith() {
+        var associated = Mappers.associateWith(List.of(1, 2, 3), it -> String.valueOf(it));
+        var expected = Map.of(1, "1", 2, "2", 3, "3");
+
+        assertEquals(expected, associated);
+    }
+
+    @Test
+    public void groupByList() {
+        var grouped = Mappers.groupBy(List.of(1, 2, 3, -1, -2, -3), it -> it < 0);
+        var expected = Map.of(true, List.of(-1, -2, -3), false, List.of(1, 2, 3));
+
+        assertEquals(expected, grouped);
+    }
+
+    @Test
+    public void groupBySet() {
+        var grouped = Mappers.groupBy(Set.of(1, 2, 3, -1, -2, -3), it -> it < 0);
+        var expected = Map.of(true, Set.of(-1, -2, -3), false, Set.of(1, 2, 3));
+
+        assertEquals(expected, grouped);
+    }
+
+    @Test
+    public void flattenList() {
+        var flattenedList = Mappers.flatten(List.of(List.of(1, 2, 3), List.of(10, 20, 30)), it -> it.stream().map(number -> number.toString()));
+        var expected = List.of("1", "2","3","10","20","30");
+
+        assertEquals(expected, flattenedList);
+    }
+
+    @Test
+    public void flattenSet() {
+        var flattenedList = Mappers.flatten(Set.of(List.of(1, 2, 3), List.of(10, 20, 30)), it -> it.stream().map(number -> number.toString()));
+        var expected = Set.of("1", "2","3","10","20","30");
+
+        assertEquals(expected, flattenedList);
+    }
+
+}
